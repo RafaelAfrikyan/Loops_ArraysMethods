a52be06d619d83008cf6572433d803994ebd13d4
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2021-12-02 23:53:06 +0400

    feat: add oddOrEven task

---
+++ oddOrEven.js
@@ -0,0 +1,8 @@
+// 1. Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+
+let number = 125;
+if(number % 2 === 0) {
+  console.log(`${number} is even`)
+} else {
+    console.log(`${number} is odd`)
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Loops and Array Methods
+
+## 1. Odd or even
+
+Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+
+[oddOrEven.js](oddOrEven.js)
